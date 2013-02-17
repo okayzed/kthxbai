4f9eb474b9ee4fc5a2365eddf83755d3d47ccb72
okay <okay.zed@gmail.com>
2013-02-17 09:25:07 -0800

    Getting started with first behaviors

--- js/app.js
+++ js/app.js
@@ -5,3 +5,40 @@ App.IndexRoute = Ember.Route.extend({
     controller.set('content', ['a', 'b', 'c']);
   }
 });
+
+App.Router.map(function() {
+  this.resource('behavior', { path: '/behaviors/:behavior_id' });
+});
+
+App.BehaviorRoute = Ember.Route.extend({
+  model: function(params) {
+    var behavior_name = (params.behavior_id || '').replace(/_/g, ' ');
+    var behavior = BEHAVIORS[behavior_name] || NO_BEHAVIOR;
+    behavior.behaviorName = behavior_name || "None";
+    return behavior;
+  }
+});
+
+// {{{ JS Data
+var NO_BEHAVIOR = { behaviorName: "None" };
+var BEHAVIORS = {
+  "the circle joker"           : {
+  },
+  "follow the leader"          : {
+  },
+  "galileo's mutterance"       : {
+  },
+  "ignorant queries"           : {
+  },
+  "non-explicit non-listening" : {
+  },
+  "success shakes"             : {
+  },
+  "the accelerated gait"       : {
+  },
+  "the false dilemma"          : {
+  },
+  "time traveling"             : {
+  },
+};
+// }}}
